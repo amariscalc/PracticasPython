aefeb111284ac152b5f016cf010594ac4217bd72
Alberto A. Mariscal <alberto.mariscal.c@gmail.com>
2023-07-24 21:55:29 +0200

    up

---
+++ tipoDatos.py
@@ -0,0 +1,27 @@
+"""
+Autor Alberto Antonio Mariscal Casado
+2023-07-24
+Python
+"""
+
+'''
+Pruebas
+de comentarios en varias lineas
+con comillas dobles y comillas simples
+'''
+
+"""Declaración de variables
+En Python para declarar variables solo debes darles nombre y asignales un valor, o un valor vacío none
+"""
+nombre = "Alberto"
+edad = 35
+lugarNacimiento = "Sevilla"
+mayorEdad = True
+altura = 1.81 
+
+#Como consultar el tipo de dato. Se utiliza la función "type()"
+print(type(nombre))
+print(type(edad))
+print(type(lugarNacimiento))
+print(type(mayorEdad))
+print(type(altura))
