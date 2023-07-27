aacf3766df720cc0e012f914758ebda6b478b834
Alberto A. Mariscal <alberto.mariscal.c@gmail.com>
2023-07-27 18:13:23 +0200

    .

--- 02_variables.py
+++ 02_variables.py
@@ -22,3 +22,16 @@ edad = str (edad)
 print (type(nombre_ciudad))
 print (type(edad))
 print (type(nombre))
+
+#Uso de la función len(), que  devuelve la "longitud" del valor de la variable
+print(len(edad), len(nombre))
+
+pi = 3.14219
+
+'''Los tipo float no son compatibles con la función len()
+print(len(pi))
+'''
+
+#Inicialización variables en una sola línea. No es muy buena práctica.
+name, alias, age, city = "Jose Luis", "Joselu", 35, "Utrera" 
+print("Me llamo",name,"tengo",age,"años y soy de",city,". Más conocido como",alias)
