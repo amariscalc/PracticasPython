f3446be8d62e1cfacc5f913292bdeeec76f728af
Alberto A. Mariscal <alberto.mariscal.c@gmail.com>
2023-07-27 16:21:24 +0200

    variables y condicional if

--- 02_variables.py
+++ 02_variables.py
@@ -4,6 +4,21 @@ La convención de nombres de variables de Python establece que lo correcto es qu
 de estar formada por varias palabras, separarlas por guión bajo "_"
 '''
 
+#Inicializamos una variables con unos valores
 nombre_ciudad = "Sevilla"
+edad = 35
+nombre = "Alberto"
 
-print (nombre_ciudad)
+#Con type() obter el tipo de variable
+print (type(nombre_ciudad))
+print (type(edad))
+print (type(nombre))
+
+#Imprimir por pantalla usando las variables anteriores, de forma concatenada
+print ("Me llamo",nombre,"soy de",nombre_ciudad,"y tengo",edad,"años")
+
+#Probar a modificar el tipo de una variable y volver a obtener los tipos
+edad = str (edad)
+print (type(nombre_ciudad))
+print (type(edad))
+print (type(nombre))
