ca89c6c156eff3bd13b1b61e742fec40f6a7b4a7
Alberto A. Mariscal <alberto.mariscal.c@gmail.com>
2023-07-27 22:42:37 +0200

    add variable operadores

---
+++ 02_02_variables_operadores.py
@@ -0,0 +1,43 @@
+'''
+Operadores.
+
+Como en "el mundo tradicional" con los números podemos sumarlos, restarlos, multiplicarlos, dividirlos,....
+y algunos operadores adicionales con los que contamos.
+Y no solo numero enteros, sino tambien número reales, números complejos...
+
+Recordatorio: Usamos print solo para que al probar los ejemplos se muestre el resultado por la salida de la terminal.
+'''
+
+### Sumar
+print ("Sumar")
+print (30 + 2)
+print (2 + 8)
+
+### Restar
+print ("Restar")
+print (10 - 6)
+print (2 - 8)
+
+### Multiplicar
+print ("Multiplicar")
+print (2 * 8)
+print (3 * 4)
+
+### Dividir
+print ("Dividir")
+print (10 / 6)
+print (3 / 4)
+
+### División con aproximación al número entero
+print ("División con aproximación")
+print(10 // 3)
+
+### Módulo (resto de una división)
+print ("Módulo")
+print (10 % 2)
+print (10 % 3)
+
+### Exponente
+print ("Exponente")
+print (2 ** 3)
+print (3 ** 3)
