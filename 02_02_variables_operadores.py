e8608bd189336fe965f934a2c3e32988d99efd91
Alberto A. Mariscal <alberto.mariscal.c@gmail.com>
2023-07-28 15:16:30 +0200

    add operadores logicos

--- 02_02_variables_operadores.py
+++ 02_02_variables_operadores.py
@@ -8,6 +8,8 @@ Y no solo numero enteros, sino tambien número reales, números complejos...
 Recordatorio: Usamos print solo para que al probar los ejemplos se muestre el resultado por la salida de la terminal.
 '''
 
+######## Operadores Aritméticos
+
 ### Sumar
 print ("Sumar")
 print (30 + 2)
@@ -41,3 +43,18 @@ print (10 % 3)
 print ("Exponente")
 print (2 ** 3)
 print (3 ** 3)
+
+######## Operadores Comparativos
+print("Menor, menor igual, mayor, mayor igual,mayor igual, igual, distinto")
+print (3 < 4) # TRUE
+print (2 <= 4) # TRUE
+print (3 > 4) # FALSE
+print (1 >= 0.5) # TRUE
+print (2 >= 2) # TRUE
+print (4 == 9) # FALSE
+print (4 != 4) # FALSE
+
+######## Operadores Lógicos
+print (3 < 4 and 5 == 5)
+print (4 < 3 or 4 != 4)
+
