21b0be776f1053ecd685d5a5d4b82a1e5f1365e3
Alberto A. Mariscal <alberto.mariscal.c@gmail.com>
2023-07-30 19:14:10 +0200

    distintas formas de formateo y print

--- 03_01_strings_formateo.py
+++ 03_01_strings_formateo.py
@@ -1,13 +1,30 @@
-# Formateo cadenas de texto
+# Formateo cadenas de texto:
 
-name, surname, edad = "Alberto", "Mariscal", 35
+# Inicializar algunas variables
+name, surname, age = "Alberto", "Mariscal", 35
 
 '''
-Usamos lo siguiente dependiendo del tipo de dato que contenga la variable
+Depende del tipo de dato que contenga la variable:
 %s string
 %d int
 %f float
 %.NumeroDigitosPuntoFlotante
 '''
 
-print ("Hola me llamo %s %s y tengo %d" %(name,surname,edad))
+# Forma simple pero poco optima para el sistema de imprimir un texto con variables.
+print ("Hola me llamo "+name+" "+surname+" y tengo "+str(age)+ " años.")
+
+# Imprime texto con las variables formateadas según el tipo de dato.
+# Mejor opción junto a la inferencia: print (f"")
+print ("Hola me llamo %s %s y tengo %d años."%(name,surname,age))
+
+# Imprime texto sin asegurar el formateo de las variables cada {} corresponde a una variable y se insertarán
+# en orden aparecen a final de linea en el paréntesis.
+print ("Hola me llamo {} {} y tengo {} años.".format(name,surname,age))
+
+'''
+Inferencia de datos
+Colocar una "f" antes de las comillas del print.
+Es la mejor opción si no vamos a formatear las variablables explicitamente 
+'''
+print (f"Hola me llamo {name} {surname} y tengo {age} años.")
