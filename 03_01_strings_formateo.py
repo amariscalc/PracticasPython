08549589476c0dcda0531fcc6fef3040e4ee86bf
Alberto A. Mariscal <alberto.mariscal.c@gmail.com>
2023-07-30 18:39:22 +0200

    Create 03_01_strings_formateo.py

---
+++ 03_01_strings_formateo.py
@@ -0,0 +1,13 @@
+# Formateo cadenas de texto
+
+name, surname, edad = "Alberto", "Mariscal", 35
+
+'''
+Usamos lo siguiente dependiendo del tipo de dato que contenga la variable
+%s string
+%d int
+%f float
+%.NumeroDigitosPuntoFlotante
+'''
+
+print ("Hola me llamo %s %s y tengo %d" %(name,surname,edad))
