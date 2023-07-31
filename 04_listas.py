4c4a588c0878fddc54c9b218fa729d3f57d5cc75
Alberto A. Mariscal <alberto.mariscal.c@gmail.com>
2023-07-31 23:55:39 +0200

    comentario salida print for

--- 04_listas.py
+++ 04_listas.py
@@ -28,3 +28,12 @@ print ("Longitud original de la lista my_other_list: %d"%(len(my_other_list)))
 # Recorrer la lista e imprimir por pantalla el item y la posición que ocupa en la lista
 for item in my_other_list:
     print ("El objeto es %d en la posición %d " %(item, my_other_list.index(item)))
+
+'''
+El print anterior muestra  debe mostrar algo similar a:
+El objeto es 35 en la posición 0 
+El objeto es 56 en la posición 1 
+El objeto es 78 en la posición 2 
+El objeto es 14 en la posición 3 
+El objeto es 25 en la posición 4
+'''
