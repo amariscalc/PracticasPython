61aab929c775eabc25489e240ca57154a92b47c5
Alberto A. Mariscal <alberto.mariscal.c@gmail.com>
2023-08-13 23:17:03 +0200

    update diccionarios

--- 07_diccionarios.py
+++ 07_diccionarios.py
@@ -49,3 +49,80 @@ if "Apellido" in my_other_dict:
     print ("%s" %(my_other_dict["Apellido"]))
 else:
     print ("No existe.")
+
+# Update. Actualizar el diccionario con un diccionario o un objeto iterable con "key,valor"
+# WARN! Es case sensitive y puede crearnos un nuevo key,valor si la key no coincide exactamente.
+print (my_other_dict) # {'Nombre': 'Alberto', 'Apellido': 'Mariscal', 'Edad': 35}
+my_other_dict.update({"Edad":40})
+print (my_other_dict) # {'Nombre': 'Alberto', 'Apellido': 'Mariscal', 'Edad': 40}
+
+# Eliminar objetos
+# pop elimina el objeto indicado
+print (my_other_dict) # {'Nombre': 'Alberto', 'Apellido': 'Mariscal', 'Edad': 40}
+print (my_other_dict.pop("Edad")) # 40
+print (my_other_dict) # {'Nombre': 'Alberto', 'Apellido': 'Mariscal'}
+
+# popitem elimina el último objeto insertado
+print (my_other_dict) # {'Nombre': 'Alberto', 'Apellido': 'Mariscal'}
+print (my_other_dict.popitem()) # ('Apellido', 'Mariscal')
+print (my_other_dict) # {'Nombre': 'Alberto'}
+
+# del sirve para eliminar item o eliminar el diccionario al completo
+# podemos eleminar un item, indicando la key
+print (my_third_dict) 
+del my_third_dict ["CCAA"]
+print (my_third_dict) 
+
+# Eliminar el diccionario si solo se indica el nombre del diccionario
+del my_third_dict
+#print (my_third_dict) # NameError: name 'my_third_dict' is not defined. Did you mean: 'my_other_dict'?
+
+# Clear limpia/vacía el diccionario.
+print (my_other_dict) # {'Nombre': 'Alberto'}
+my_other_dict.clear()
+print (my_other_dict) # {}
+
+# Loop for en un diccionario
+coches = dict ()
+coches = {
+    id : 1,
+    "Marca" : "Seat",
+    "Modelo": "Ateca",
+    "Color" : "Verde",
+    "Paquete": "Premium"
+}
+for x,y in coches.items():
+    print ("%s %s"%(x,y))
+
+# Copiar un diccionario
+# Copy
+vehiculos = coches.copy()
+print (type(vehiculos)) # <class 'dict'>
+print (vehiculos) # {<built-in function id>: 1, 'Marca': 'Seat', 'Modelo': 'Ateca', 'Color': 'Verde', 'Paquete': 'Premium'}
+# Copiar diccionario con dict ()
+new_vehiculos = dict (vehiculos)
+print (new_vehiculos) # {<built-in function id>: 1, 'Marca': 'Seat', 'Modelo': 'Ateca', 'Color': 'Verde', 'Paquete': 'Premium'}
+
+# Anidación de diccionarios, es cuando un diccionario contiene diccionarios
+provincias = dict ()
+provincias = {
+    "Sevilla" : {
+        "Capital":"Sevilla",
+        "Habitantes":1947852,
+    },
+    "Huelva" : {
+        "Capital":"Huelva",
+        "Habitantes":525835,
+    },
+    "Cádiz" : {
+        "Capital":"Cádiz",
+        "Habitantes":1245960,
+    }
+}
+# Recorremos el diccionario y guardamos las key y valor en dos variables
+x = ""
+y = "" 
+for x in provincias.items():
+        print ("Provincia ",x[0])
+        for y in x[1].items():
+            print ("\t %s %s. "%(y[0], y[1]))
