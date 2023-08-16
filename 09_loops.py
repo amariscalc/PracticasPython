6724570ac220c73eb6cbc78f11954065ef3b9228
Alberto A. Mariscal <alberto.mariscal.c@gmail.com>
2023-08-16 22:36:06 +0200

    update loops

---
+++ 09_loops.py
@@ -0,0 +1,13 @@
+# Loops / Bucles
+
+'''
+Son mecanismo o estructuras que nos permiten iterar de forma reiterada.
+Son sirve para pasar por el mismo código varias veces. 
+'''
+
+### While
+i = 0
+while i < 10:
+    print ("Bucle while con i valor %d"%(i))
+    i +=1
+print ("Fin del bucle While.")
