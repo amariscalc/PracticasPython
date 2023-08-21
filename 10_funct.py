31086935a2c63fc38cefd1877bdac3f9205bd2a0
Alberto A. Mariscal <alberto.mariscal.c@gmail.com>
2023-08-21 20:08:56 +0200

    update

--- 10_funct.py
+++ 10_funct.py
@@ -5,13 +5,13 @@ cuya finalidad es realizar una tarea específica.
 Puede reutilizarse a voluntad para repetir dicha tarea.
 '''
 
-### Definir función que pide por teclado dos números
+### Pide por teclado dos números
 def set_numbers ():
     first_number =float (input("Introduce el primer número: "))
     second_number =float (input("Introduce el segundo número: "))
     return first_number,second_number
 
-### Definir una función que recibe dos parámetros, una tupla de dos números y un operador matemático
+### Recibe dos parámetros, una tupla de dos números y un operador matemático
 def calculate (numbers,operator):
     match str.lower(operator):
         case "+":
@@ -27,7 +27,7 @@ def calculate (numbers,operator):
             total = numbers [0] / numbers [1]
             return total
 
-### Función que pide al usuario el operador y lo valida
+### Pide al usuario el operador y lo valida
 def operator_simbol ():
     operator = input ("Selecciona operación + - * / : ")
     while not validate_operation (operator):
@@ -35,14 +35,14 @@ def operator_simbol ():
         operator = input ("Selecciona operación + - * / : ")
     return operator
 
-### Función que valida que el operador introducido es uno de los 4 admitidos
+### Valida que el operador introducido es uno de los 4 admitidos
 def validate_operation (operator):
     if operator == "+" or operator == "-" or operator == "*" or operator == "/":
         return True
     else:
         return False
     
-### Función consultar si el usuario desea salir de la calculadora o volver a empezar.
+### Consultar si el usuario desea salir de la calculadora o volver a empezar.
 def exit_cal ():
     exit_request = input ("¿Desea salir? Si / No ")
     exit_request_lower = exit_request.lower()
@@ -58,8 +58,8 @@ print ("Bienvenido/a a la calculadora.")
 while not exit_calc_app :
     # Solicitar dos numeros por teclado:
     numbers = set_numbers ()
-    # Solicita la operación a realizar y llama a la función que comprueba el dato introducido, si no es correcto
-    # vuelve a pedir por teclado que se introduzca el operador.
+    # Solicita la operación a realizar y verifica que el símbolo introducido es correcto, si no lo es
+    # vuelve a pedir por teclado que se introduzca
     operator = operator_simbol ()
     # Guardar el resultado calculado
     total = calculate (numbers,operator)
