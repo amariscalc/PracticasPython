186ad89b29c34804d293ea760445ce7d0a0dab44
Alberto A. Mariscal <alberto.mariscal.c@gmail.com>
2023-08-25 02:43:06 +0200

    update

--- Retos/Interactuar_fichero_txt/edit.py
+++ Retos/Interactuar_fichero_txt/edit.py
@@ -3,6 +3,7 @@
 # Import os module
 import os
 
+# it´s a simple menu.
 def menu ():
     option = "0"
     while option != "1" and option != "2":
@@ -17,13 +18,15 @@ def menu ():
             print ("Option Error. Try again.")
         elif option == "1" or option == "2":
             return option
-        
+
+# Same funct to create and delete. If the file exist,it´s delete and create again.
 def to_create_delete(file):
     file_txt = open (file,"w")
     text = input ("")
     print (file_txt.readlines)
     file_txt.close ()
 
+# A funct to write the file text.txt
 def to_write (file):
     while True:
         to_read (file)
@@ -36,18 +39,21 @@ def to_write (file):
             file_write.close()
             return False
             break
-        
+
+# A funct to read the file text.txt
 def to_read (file):
     file_to_read = open (file,'r')
     text_to_read = file_to_read.read ()
     print (text_to_read)
     file_to_read.close ()
 
-# Check exist the file
+# main ()
 file_to_edit = "text.txt"
 
 print ("Welcome to the POC Python Text Editor.")
 
+# Check file exist or not
+# The file was created before
 if os.path.exists("text.txt"):
     
     option = menu ()
@@ -58,6 +64,7 @@ if os.path.exists("text.txt"):
         case "2":
             to_create_delete (file_to_edit)
             to_write (file_to_edit)
+# If file not exist
 else:
     file = input ("Create the new file. Name:")
     to_create_delete (file_to_edit)
